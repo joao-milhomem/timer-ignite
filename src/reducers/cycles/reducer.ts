71f595130e1f068ad355d5043b583f42556d4d96
user.email <milhomemjoao.work@gmail.com>
2024-04-30 15:32:27 -0300

    lib immer para manipular os estados

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { produce } from 'immer'
 import { CycleProps } from '../../contexts/CycleContextProvider'
 
 interface StateCyclesProps {
@@ -12,48 +13,39 @@ export enum ActionTypes {
   COMPLETE_CYCLE = 'COMPLETE_CYCLE',
 }
 
-// interface ActionProps {
-//   type: ActionTypes
-//   payload: CycleProps
-// }
-
 export function cyclesReducer(state: StateCyclesProps, action: any) {
   switch (action.type) {
     case ActionTypes.CREATE_NEW_CYCLE:
-      return {
-        cycles: [...state.cycles, action.payload],
-        currentCycleId: action.payload.id,
-      }
+      return produce(state, (draft) => {
+        draft.cycles.push(action.payload)
+        draft.currentCycleId = action.payload.id
+      })
 
     case ActionTypes.SHUTDOWN_CYCLE:
-      return {
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === state.currentCycleId) {
-            return {
-              ...cycle,
-              isActive: false,
-              shutDownDate: new Date(),
-            }
-          }
-          return cycle
-        }),
-        currentCycleId: undefined,
-      }
+      return produce(state, (draft) => {
+        const cycleToBeFinished = draft.cycles.find(
+          (cycle) => cycle.id === state.currentCycleId,
+        )
+
+        if (cycleToBeFinished) {
+          cycleToBeFinished.isActive = false
+          cycleToBeFinished.shutDownDate = new Date()
+          draft.currentCycleId = undefined
+        }
+      })
 
     case ActionTypes.COMPLETE_CYCLE:
-      return {
-        cycles: state.cycles.map((cycle) => {
-          if (cycle.id === action.payload.id) {
-            return {
-              ...cycle,
-              isActive: false,
-              endDate: new Date(),
-            }
-          }
-          return cycle
-        }),
-        currentCycleId: undefined,
-      }
+      return produce(state, (draft) => {
+        const cycleCompleted = draft.cycles.find(
+          (cycle) => cycle.id === state.currentCycleId,
+        )
+
+        if (cycleCompleted) {
+          cycleCompleted.isActive = false
+          cycleCompleted.endDate = new Date()
+          draft.currentCycleId = undefined
+        }
+      })
 
     default:
       return state
